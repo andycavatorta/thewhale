adfb16a8d1c95186e5fc91ce57803e0edf36b26f
tcoor8325 <tcoor8325@gmail.com>
2022-06-20 16:24:22 -0400

    testing

--- RaspberryPiDeadManSwitch.py
+++ RaspberryPiDeadManSwitch.py
@@ -3,10 +3,12 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-count = 0
-while count < 50:
-    GPIO.output(8, GPIO.HIGH)
-    sleep(0.1)
-    GPIO.output(8, GPIO.LOW)
-    sleep(0.1)
-    count+=1
+# count = 0
+# while count < 50:
+#    GPIO.output(8, GPIO.HIGH)
+#    sleep(0.1)
+#    GPIO.output(8, GPIO.LOW)
+#    sleep(0.1)
+#    count+=1
+
+GPIO.output(8, GPIO.HIGH)
