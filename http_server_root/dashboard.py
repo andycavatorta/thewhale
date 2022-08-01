327f913379eed040e3e24a5cb6c5cf1544653cd1
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-01 18:40:45 -0400

    dash

--- http_server_root/dashboard.py
+++ http_server_root/dashboard.py
@@ -15,9 +15,9 @@ class SimpleChat(WebSocket):
 
     def handleMessage(self):
        #print("got ws message", self.data)
-       print("handleMessage",self.data)
+       print("handleMessage",(self.data["topic"], self.data["message"], "dashboard", self.data["target"]))
        #upstream_queue.put((self.data["topic"], self.data["message"], "dashboard", self.data["target"]))
-       upstream_queue
+       #upstream_queue.put
        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
     def handleConnected(self):
