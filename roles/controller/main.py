8c1eb37f9c68da8a87e9c0368a6df9ed98c10ed8
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-01 19:16:03 -0400

    dash

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -221,7 +221,8 @@ class Main(threading.Thread):
                     continue
                 print(topic, message, origin, destination)
                 self.hosts.dispatch(topic, message, origin, destination)
-                self.dashboard(codecs.decode(topic,'UTF-8'), message, origin, destination)
+                if origin != "dashboard":
+                    self.dashboard(codecs.decode(topic,'UTF-8'), message, origin, destination)
                 #self.current_mode.add_to_queue(topic, message, origin, destination)
             except Exception as e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
