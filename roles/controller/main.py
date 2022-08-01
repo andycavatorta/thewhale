b57224b01d5b2dd4bb1b08298236103d2de49044
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-01 18:30:03 -0400

    dash

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -163,7 +163,7 @@ class Main(threading.Thread):
         self.current_mode = self.modes["waiting_for_connections"]
         self.current_mode.begin()
         """
-        self.dashboard = dashboard.init(self.add_to_queue)
+        self.dashboard = dashboard.init(self.queue)
         self.start()
         self.poller = Poller(self.tb)
 
