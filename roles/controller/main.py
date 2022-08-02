4ecf9b8c1c63d23996c8121f39219830b576928c
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-02 14:55:42 -0400

    dash

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -69,6 +69,7 @@ class Poller(threading.Thread):
             time.sleep(5)
             self.tb.publish("request_computer_start_status","")
             self.tb.publish("request_computer_runtime_status","")
+            self.tb.publish("request_sdc_start_status","")
             self.upstream_queue(b"request_computer_start_status", "", "controller", "controller")
             self.upstream_queue(b"request_computer_runtime_status", "", "controller", "controller")
 
