b9576de65519aad7b85a52314557b274e6c1f2eb
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-02 19:45:56 -0400

    dash

--- http_server_root/dash.js
+++ http_server_root/dash.js
@@ -43,7 +43,7 @@ const block_grid_x = [
   1700,
   1800,
   1900,
-  2050,
+  2250,
   ];
 const block_grid_y = [
   0,
@@ -760,21 +760,21 @@ function init() {
   //status_details = new Details_Display(canvas, [20,500], "exception_details_rect")
   //msg_details = new Details_Display(canvas, [20,500], "exception_details_rect")
 
-  new Block_Title_Horizontal(canvas, [block_grid_x[1],block_grid_y[10]], "driver")
-  new Block_Title_Horizontal(canvas, [block_grid_x[2],block_grid_y[10]], "voltage")
-  new Block_Title_Horizontal(canvas, [block_grid_x[3],block_grid_y[10]], "firmware")
+  //new Block_Title_Horizontal(canvas, [block_grid_x[1],block_grid_y[10]], "driver")
+  new Block_Title_Horizontal(canvas, [block_grid_x[2],block_grid_y[10]], "24V")
+  new Block_Title_Horizontal(canvas, [block_grid_x[3],block_grid_y[10]], "5V")
   new Block_Title_Horizontal(canvas, [block_grid_x[4],block_grid_y[10]], "emergency stop")
   new Block_Title_Horizontal(canvas, [block_grid_x[5],block_grid_y[10]], "rotor")
-  new Block_Title_Horizontal(canvas, [block_grid_x[6],block_grid_y[10]], "rpm")
-  new Block_Title_Horizontal(canvas, [block_grid_x[7],block_grid_y[10]], "duty cycle")
-  new Block_Title_Horizontal(canvas, [block_grid_x[8],block_grid_y[10]], "loop error")
-  new Block_Title_Horizontal(canvas, [block_grid_x[9],block_grid_y[10]], "speed+")
-  new Block_Title_Horizontal(canvas, [block_grid_x[10],block_grid_y[10]], "speed-")
-  new Block_Title_Horizontal(canvas, [block_grid_x[11],block_grid_y[10]], "temp")
-  new Block_Title_Horizontal(canvas, [block_grid_x[12],block_grid_y[10]], "prop")
-  new Block_Title_Horizontal(canvas, [block_grid_x[13],block_grid_y[10]], "diff")
+  new Block_Title_Horizontal(canvas, [block_grid_x[6],block_grid_y[10]], "speed +")
+  new Block_Title_Horizontal(canvas, [block_grid_x[7],block_grid_y[10]], "speed -")
+  new Block_Title_Horizontal(canvas, [block_grid_x[8],block_grid_y[10]], "duty cycle")
+  new Block_Title_Horizontal(canvas, [block_grid_x[9],block_grid_y[10]], "rpm")
+  new Block_Title_Horizontal(canvas, [block_grid_x[10],block_grid_y[10]], "loop error")
+  new Block_Title_Horizontal(canvas, [block_grid_x[11],block_grid_y[10]], "PID")
+  //new Block_Title_Horizontal(canvas, [block_grid_x[12],block_grid_y[10]], "I")
+  new Block_Title_Horizontal(canvas, [block_grid_x[13],block_grid_y[10]], "mode")
   new Block_Title_Horizontal(canvas, [block_grid_x[14],block_grid_y[10]], "ppr")
-  new Block_Title_Horizontal(canvas, [block_grid_x[15],block_grid_y[10]], "mode")
+  new Block_Title_Horizontal(canvas, [block_grid_x[15],block_grid_y[10]], "driver")
 
   controllers["rotors0102"] = new SDCRow("rotors0102","rotor01","rotor02", block_grid_y[11], block_grid_y[12])
   controllers["rotors0304"] = new SDCRow("rotors0304","rotor03","rotor04", block_grid_y[13], block_grid_y[14])
