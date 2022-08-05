ceb5105ecd96339fbe8588f542b902dc636794e5
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-04 20:43:40 -0400

    dash

--- http_server_root/dash.js
+++ http_server_root/dash.js
@@ -718,15 +718,18 @@ class SDCRow{
     this.rotor1name = new Block_Display_Text(this.dom_parent, [block_grid_x[5],y_position_1], rotor1name, 100)
     this.rotor2name = new Block_Display_Text(this.dom_parent, [block_grid_x[5],y_position_2], rotor2name, 100)
 
-    this.decrease_speed_1 = new Block_Push_Button(this.dom_parent, rotor1name, [block_grid_x[6],y_position_1], "decrement_speed", 140)
-    this.decrease_speed_2 = new Block_Push_Button(this.dom_parent, rotor2name, [block_grid_x[6],y_position_2], "decrement_speed", 140)
     //this.decrease_speed_1 = new Block_Display_Text(this.dom_parent, [block_grid_x[6],y_position_1], "-", 140)
     //this.decrease_speed_2 = new Block_Display_Text(this.dom_parent, [block_grid_x[6],y_position_2], "-", 140)
     this.requested_speed_1 = new Block_Display_Text(this.dom_parent, [block_grid_x[7],y_position_1], "?", 100)
     this.requested_speed_2 = new Block_Display_Text(this.dom_parent, [block_grid_x[7],y_position_2], "?", 100)
-    this.increase_speed_1 = new Block_Display_Text(this.dom_parent, [block_grid_x[8],y_position_1], "+", 100)
-    this.increase_speed_2 = new Block_Display_Text(this.dom_parent, [block_grid_x[8],y_position_2], "+", 100)
-
+    //this.increase_speed_1 = new Block_Display_Text(this.dom_parent, [block_grid_x[8],y_position_1], "+", 100)
+    //this.increase_speed_2 = new Block_Display_Text(this.dom_parent, [block_grid_x[8],y_position_2], "+", 100)
+    this.decrease_speed_1 = new Block_Push_Button(this.dom_parent, rotor1name, [block_grid_x[6],y_position_1], "decrement", 140)
+    this.decrease_speed_2 = new Block_Push_Button(this.dom_parent, rotor2name, [block_grid_x[6],y_position_2], "decrement", 140)
+    this.requested_speed_1 = new Block_Push_Button(this.dom_parent, rotor1name, [block_grid_x[7],y_position_1], "stop", 100)
+    this.requested_speed_2 = new Block_Push_Button(this.dom_parent, rotor2name, [block_grid_x[7],y_position_2], "stop", 100)
+    this.increase_speed_1 = new Block_Push_Button(this.dom_parent, rotor1name, [block_grid_x[8],y_position_1], "increment", 100)
+    this.increase_speed_2 = new Block_Push_Button(this.dom_parent, rotor2name, [block_grid_x[8],y_position_2], "increment", 100)
 
     this.duty_cycle_1 = new Block_Display_Text(this.dom_parent, [block_grid_x[9],y_position_1], "", 100)
     this.duty_cycle_2 = new Block_Display_Text(this.dom_parent, [block_grid_x[9],y_position_2], "", 100)
