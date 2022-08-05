99b0559816908720145b96e24b50b419b0aca8b4
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-05 12:29:28 -0400

    dash1

--- roles/rotor/main.py
+++ roles/rotor/main.py
@@ -146,7 +146,7 @@ class Main(threading.Thread):
             "encoder_speed_relative_1":self.sdc.motor_1.get_encoder_speed_relative(),
             "encoder_speed_relative_2":self.sdc.motor_1.get_encoder_speed_relative(),
             "motor_command_applied_1":self.sdc.motor_1.get_motor_command_applied(),
-            "motor_command_applied_2":self.sdc.motor_1.get_motor_command_applied(),
+            "motor_command_applied_2":self.sdc.motor_2.get_motor_command_applied(),
             "current_time":time.time()
         }
 
