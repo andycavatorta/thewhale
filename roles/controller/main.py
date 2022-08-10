1f8872ba00aa2f3ea268d20dcf5d7d9d6d4f2ba6
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-09 22:06:15 -0400

    adding and testing speeds

--- roles/controller/main.py
+++ roles/controller/main.py
@@ -213,8 +213,8 @@ class Main(threading.Thread):
         self.poller = Poller(self.tb, self.add_to_queue)
 
 
-    def handle_dashboard_note_buttons(self,topic, message, origin, destination):
-        print("handle_dashboard_note_buttons",topic, message, origin, destination)
+    def handle_dashboard_note_buttons(self, topic, message, origin, destination):
+        print("handle_dashboard_note_buttons", topic, message, origin, destination)
 
     def map_pitch_to_rotor(self,pitch_str):
         pass
