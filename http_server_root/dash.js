ad0613cdbd99c2b1636c4389415b710976b5bd15
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-29 17:59:22 -0400

    modifying dash

--- http_server_root/dash.js
+++ http_server_root/dash.js
@@ -379,13 +379,14 @@ class Grid_Folding{
         let column = column_group["columns"][column_index];
         this.columns[column["title"]]["title"].setAttribute("y", "100px");
         this.columns[column["title"]]["title"].setAttribute("x", left + `px`);
+        let y = 160
         for (const row_number of Array(14).keys()){
-          let y = row_name_lookup[row_number]["y"]
           this.columns[column["title"]][row_number].text_container.setAttribute("y", y + `px`);
           this.columns[column["title"]][row_number].text_container.setAttribute("x", left + `px`);
           this.columns[column["title"]][row_number].button_rect.setAttribute("y", y + `px`);
           this.columns[column["title"]][row_number].button_rect.setAttribute("x", left + `px`);
           this.columns[column["title"]][row_number].button_rect.setAttribute("width", column["width"] + `px`);
+          y = y + 40;
         }
 
         console.log(this.columns[column["title"]])
