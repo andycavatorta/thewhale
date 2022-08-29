6a7a8785e8e880f1c15a9bd2899dff2eaf451c6b
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-29 11:50:18 -0400

    modifying dash

--- http_server_root/dash.js
+++ http_server_root/dash.js
@@ -286,7 +286,7 @@ class Grid_Folding{
   update_layout() {
     var left = 0
     for (let column_group_index in this.column_groups_a) {
-      let column_group = column_groups_a[column_group_index];
+      let column_group = this.column_groups_a[column_group_index];
       for (let column_index in column_group["columns"]) {
         let column = column_group["columns"][column_index];
         console.log(column["title"]["width"])
