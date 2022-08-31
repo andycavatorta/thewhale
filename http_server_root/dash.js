ae6d0bc49000d48b895bb5a78b9930ec7dac68db
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-31 17:11:53 -0400

    refactor11

--- http_server_root/dash.js
+++ http_server_root/dash.js
@@ -487,7 +487,7 @@ class Grid_Folding{
       for (let column_index in column_group["columns"]) {
         var column = column_group["columns"][column_index];
         if (column_group["folded"]) {
-          var _width_ = 0;
+          var _width_ = 36;
         }else{
           var _width_ = column["width"];
         }
