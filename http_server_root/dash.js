5a2dbdc2bb88ef1a1c03bee6de3b1bffe63e06bc
Andy Cavatorta <hellyeah@media.mit.edu>
2022-08-31 15:18:51 -0400

    refactor1

--- http_server_root/dash.js
+++ http_server_root/dash.js
@@ -456,7 +456,8 @@ class Grid_Folding{
 
     this.update_layout()
 
-    for (var row in name_row_lookup){
+    for (var row_name in name_row_lookup){
+      row = name_row_lookup[row_name]
       console.log("==============", row, row[0])
       this.update_data(row,"computer_name", row_name_lookup[row[0]]["computer_name"])
       this.update_data(row,"mcu_name", row_name_lookup[row[0]]["mcu_name"])
